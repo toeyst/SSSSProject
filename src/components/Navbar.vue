fb4e7b73b53d007bdbcdc47a9cc90917431583ad
toeyst <toeysmile5678@hotmail.com>
2023-01-05 15:54:57 +0700

    import bootstrap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,38 +11,28 @@ defineProps({
 <template>
 <header :class="{'scrolled-nav': scrollPosition}">
 <nav>
-<div class="nav">
-  <RouterLink to="/">Home   </RouterLink>
-  <RouterLink to="/about">   Contract Us </RouterLink> 
-  <RouterLink to="/">    Login</RouterLink>
 
-</div>
 <div class="container">
   <div class="row">
-    <div class="col">
-      Column
+    <div class="col-6">
+      <RouterLink to="/">Home   </RouterLink>
+    <RouterLink to="/about">   Contract Us </RouterLink> 
+    <RouterLink to="/">    Login</RouterLink>
     </div>
-    <div class="col">
-      Column
+    <div class="col-6">
+      <RouterLink to="/">Home   </RouterLink>
+    <RouterLink to="/about">   Contract Us </RouterLink> 
+    <RouterLink to="/">    Login</RouterLink>
     </div>
-    <div class="col">
-      Column
-    </div>
+    
   </div>
 </div>
 
-
-
-
-
 </nav>
+
 </header>
 </template>
 
 <style scoped>
-.nav{
-  width: 500px;
-  background-color: black;
-  color: white;
-}
+
 </style>
